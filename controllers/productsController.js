b19c7f8fc15dcd96a0348dc465c5f00df6b845c1
Amey Pandit <amey.pandit@sportzinteractive.com>
2024-08-22 11:05:38 +0530

    added getproductbyid

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,22 +1,36 @@
 import pool from "../db/db.js";
 
 const getProducts = async (req, res) => {
-    try {
-       const selectProductsQuery = "SELECT * FROM amey.products";
+  try {
+    const selectProductsQuery = "SELECT * FROM amey.products";
 
-       const products = await pool.query(selectProductsQuery);
-       if(products.rowCount>1 ){
-        return res.status(200).json(products.rows);
-       }
-       else {
-        return res.status(404).json({error: 'No products found'})
-       }
-    } 
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({error: 'Something went wrong'})
+    const products = await pool.query(selectProductsQuery);
+    if (products.rowCount > 1) {
+      return res.status(200).json(products.rows);
+    } else {
+      return res.status(404).json({ error: "No products found" });
     }
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
 
+const getProductsById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const selectProductQuery = `SELECT * FROM amey.products WHERE product_id = ${id}`;
+    const products = await pool.query(selectProductQuery);
 
-export { getProducts };
+    if (products.rowCount > 0) {
+      return res.status(200).json(products.rows);
+    } else {
+      return res.status(404).json({ error: "No product found" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+export { getProducts, getProductsById };
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getProducts } from '../controllers/productsController.js';
+import { getProducts, getProductsById } from '../controllers/productsController.js';
 
 const productsRouter = express.Router();
 
 productsRouter.get('/',getProducts);
+
+productsRouter.get('/:id',getProductsById);
 
 export default productsRouter;

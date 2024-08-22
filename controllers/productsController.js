4eac8d0fcf41444f4c1b61afd334e3fcd2ac38a0
Amey Pandit <amey.pandit@sportzinteractive.com>
2024-08-22 12:17:22 +0530

    get by id, category and price range added

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,7 +5,7 @@ const getProducts = async (req, res) => {
     const selectProductsQuery = "SELECT * FROM amey.products";
 
     const products = await pool.query(selectProductsQuery);
-    if (products.rowCount > 1) {
+    if (products.rowCount > 0) {
       return res.status(200).json(products.rows);
     } else {
       return res.status(404).json({ error: "No products found" });
@@ -33,4 +33,55 @@ const getProductsById = async (req, res) => {
   }
 };
 
-export { getProducts, getProductsById };
+const deleteById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = `DELETE FROM amey.products WHERE product_id = ${id}`;
+    const response = await pool.query(query);
+    if (response.rowCount == 1) {
+      return res.status(200).json({ message: "Product deleted successfully" });
+    } else {
+      return res.status(404).json({ error: "No product found" });
+    }
+
+    return res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+const getProductsByCategory = async (req, res) => {
+  try {
+    const category = req.query.category;
+    const selectQuery = `select * from amey.products where category ILIKE '${category}'`;
+    const result = await pool.query(selectQuery);
+    if (result.rowCount > 0) {
+      return res.status(200).json(result.rows);
+    } else {
+      return res.status(404).json({ error: "No products found" });
+    }
+  } catch (error) {
+    console.log("Error Caught " + error?.message);
+    return res.status(500).json({ error: "Internal Error" });
+  }
+};
+
+const getProductsByPriceRange = async (req, res) => {
+    try {
+      const min = req.query.min;
+      const max = req.query.max;
+      const selectQuery = `select * from amey.products where price between ${min} and ${max}`;
+      const result = await pool.query(selectQuery);
+      if (result.rowCount > 0) {
+        return res.status(200).json(result.rows);
+      } else {
+        return res.status(404).json({ error: "No products found" });
+      }
+    } catch (error) {
+      console.log("Error Caught " + error?.message);
+      return res.status(500).json({ error: "Internal Error" });
+    }
+  };
+
+export { getProducts, getProductsById, deleteById, getProductsByCategory, getProductsByPriceRange };
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,10 +1,22 @@
-import express from 'express';
-import { getProducts, getProductsById } from '../controllers/productsController.js';
+import express from "express";
+import {
+  deleteById,
+  getProducts,
+  getProductsById,
+  getProductsByCategory,
+  getProductsByPriceRange,
+} from "../controllers/productsController.js";
 
 const productsRouter = express.Router();
 
-productsRouter.get('/',getProducts);
+productsRouter.get("/", getProducts);
 
-productsRouter.get('/:id',getProductsById);
+productsRouter.get("/id/:id", getProductsById);
+
+productsRouter.delete("/id/:id", deleteById);
+
+productsRouter.get("/category", getProductsByCategory);
+
+productsRouter.get("/price", getProductsByPriceRange);
 
 export default productsRouter;

083ac5092496bfd50487d47c613c615387f09cae
Amey Pandit <amey.pandit@sportzinteractive.com>
2024-08-23 16:17:58 +0530

    Testing fixed

--- test/productsMockController.test.js
+++ test/productsMockController.test.js
@@ -1,28 +1,28 @@
-import sinon from "sinon";
-import pool from "../db/db.js";
-
-import app from "../index.js";
-import { use, expect, should } from "chai";
+import { expect, should, use } from "chai";
 import chaiHttp from "chai-http";
+import sinon from "sinon";
+import app from "../index.js";
+import pool from "../db/db.js";
 
 const chai = use(chaiHttp);
 should();
 
-const url = `/products`;
+const url = "/products";
 
 describe("Mocking product controller", () => {
   afterEach(() => {
-    sinon.restore(); //Restore Original Methods
+    sinon.restore(); // Restore original methods
   });
 
-  //1. Test for Empty Response (No Products Found)
-  it("Returns 404 when no Products are found", () => {
+  // 1. Test for Empty response (No Products Found)
+
+  it("returns 404 when no products are found", async () => {
     sinon.stub(pool, "query").resolves({ rowCount: 0 });
-    const result = chai.request(app).get("/products");
-    expect(result).to.have.status(400);
+    const result = await chai.request.execute(app).get(url);
+    expect(result).to.have.status(404);
   });
 
-  it("should return 200 and product data when products are found", () => {
+  it("should return 200 and product data when products are found", async () => {
     const mockData = [
       {
         product_id: 1,
@@ -31,23 +31,45 @@ describe("Mocking product controller", () => {
         category: "WaterSports",
         star_rating: 4,
         description: "of Lorem Ipsum available",
-        imageurl: "assets/images/bottle.jpeg",
+        productcode: "WAT-BOT",
+        imageurl: "bottle.jpeg",
       },
     ];
     sinon
       .stub(pool, "query")
       .resolves({ rowCount: mockData.length, rows: mockData });
 
-    const res = chai.request(app).get("/products");
+    const res = await chai.request.execute(app).get("/products");
     expect(res).to.have.status(200);
-    expect(res.body).to.be.an("array").that.deep.equals(mockData);
+    expect(res.body).to.be.an("array").that.deep.equal("Internal Error");
   });
 
-  it("should return 500 for internal server error", () => {
-    sinon.stub(pool, "query").rejects(new Error("Database Error"));
+  //3. Checking it returns 500 when there is internal error (For eg. db connection rejected)
 
-    const res = chai.request(app).get("/products");
-    expect(res).to.have.status(500);
-    expect(res.body).to.have.property("error").to.equal("Internal Error");
+  it("should return 500 for internal error", async () => {
+    sinon.stub(pool, "query").rejects(new Error("Internal Error"));
+    const result = await chai.request.execute(app).get(url);
+    expect(result).to.have.status(500);
+  });
+});
+
+describe("Testing getById end point", () => {
+  afterEach(() => {
+    sinon.restore(); // Restore original methods
+  });
+  //OK Status 200
+
+  it("should return 200 and product data when a valid ID is provided", async () => {
+    const mockData = {
+      product_id: 1,
+      productname: "WaterBottle",
+      price: 40,
+      category: "WaterSports",
+      star_rating: 4,
+      description: "of Lorem Ipsum available",
+      productcode: "WAT-BOT",
+      imageurl: "bottle.jpeg",
+    };
+    sinon.stub(pool, "query").resolves({ rowCount: 1, rows: [mockData] });
   });
 });
